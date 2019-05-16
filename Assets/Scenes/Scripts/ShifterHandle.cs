eeaf890d7554f4e4c518aee0555b2a194ee1ca2d
Tsubasa Namiki <namikingsoft@gmail.com>
2019-05-17 07:08:28 +0900

    refactor: use Vector2 instead of Vector3

--- Assets/Scenes/Scripts/ShifterHandle.cs
+++ Assets/Scenes/Scripts/ShifterHandle.cs
@@ -9,26 +9,33 @@ public class ShifterHandle : MonoBehaviour
     private Vector2 previousPosition = new Vector2(0, 0);
     private float moveFactor = 1; // 0-1
 
+    private Transform handleTransform;
+
+    private void Start()
+    {
+        handleTransform = transform.Find("Handle");
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         moveFactor = moveFactor + Time.deltaTime * 6;
         if (moveFactor >= 1) moveFactor = 1;
-        transform.Find("Handle").localPosition = Vector2.Lerp(previousPosition, currentPosition, moveFactor);
+        handleTransform.localPosition = Vector2.Lerp(previousPosition, currentPosition, moveFactor);
     }
 
     public void Shift(int gear)
     {
         if (gear == currentGear) return;
         currentGear = gear;
-        previousPosition = new Vector3(currentPosition.x, currentPosition.y);
-        if (gear == 0) currentPosition = new Vector3(0, 0, 0);
-        else if (gear == 1) currentPosition = new Vector3(-23, 38, 0);
-        else if (gear == 2) currentPosition = new Vector3(-23, -38, 0);
-        else if (gear == 3) currentPosition = new Vector3(0, 38, 0);
-        else if (gear == 4) currentPosition = new Vector3(0, -38, 0);
-        else if (gear == 5) currentPosition = new Vector3(23, 38, 0);
-        else if (gear == 6) currentPosition = new Vector3(23, -38, 0);
+        previousPosition = new Vector2(currentPosition.x, currentPosition.y);
+        if (gear == 0) currentPosition = new Vector2(0, 0);
+        else if (gear == 1) currentPosition = new Vector2(-23, 38);
+        else if (gear == 2) currentPosition = new Vector2(-23, -38);
+        else if (gear == 3) currentPosition = new Vector2(0, 38);
+        else if (gear == 4) currentPosition = new Vector2(0, -38);
+        else if (gear == 5) currentPosition = new Vector2(23, 38);
+        else if (gear == 6) currentPosition = new Vector2(23, -38);
         moveFactor = 0;
     }
 }
